195149d677ea8b1261213be7d6e1f6ad99cd1035
mpaulweeks <mpaulweeks@gmail.com>
2020-02-11 13:44:48 -0500

    downloading & unzipping

--- bin/fetchVersion.ts
+++ bin/fetchVersion.ts
@@ -1,19 +1,49 @@
-const fs = require('fs').promises;
+const decompress = require('decompress');
+const deleter = require('delete');
 const fetch = require('node-fetch');
+const fs = require('fs');
+const fsPromises = fs.promises;
 
 interface Version {
   version: string;
   updated: string;
 }
+const path = {
+  latestDist: 'latest/dist',
+  latestVersion: 'latest/version.json',
+  tempDist: 'tmp/dist.zip',
+  remoteVersion: 'http://storage.googleapis.com/fighter-html/version.json',
+  remoteDist: (version: string) => `http://storage.googleapis.com/fighter-html/${version}.zip`,
+};
+
+const downloadDist = async (version: string) => {
+  console.log('downloading:', version);
+  const latestResp = await fetch(path.remoteDist(version));
+
+  const stream = fs.createWriteStream(path.tempDist);
+  await new Promise((resolve, reject) => {
+    stream.on('error', reject);
+    stream.on('finish', resolve);
+    latestResp.body.pipe(stream);
+  });
+  console.log('finished download');
+};
+
+const unzipDist = async () => {
+  console.log('unzipping...');
+  await deleter.promise([path.latestDist]);
+  await decompress(path.tempDist, path.latestDist);
+  await deleter.promise([path.tempDist]);
+}
 
 const fetchVersion = async () => {
-  const versionRep = await fetch('http://storage.googleapis.com/fighter-html/version.json');
+  const versionRep = await fetch(path.remoteVersion);
   const newVersionRaw = await versionRep.text();
   const newVersion: Version = JSON.parse(newVersionRaw);
 
   let oldVersion: (Version | undefined);
   try {
-    const oldVersionRaw = await fs.readFile('latest/version.json');
+    const oldVersionRaw = await fsPromises.readFile(path.latestVersion);
     oldVersion = JSON.parse(oldVersionRaw);
   } catch (e) {
     // do nothing
@@ -21,16 +51,15 @@ const fetchVersion = async () => {
 
   console.log(oldVersion, newVersion);
   if (!oldVersion || oldVersion.version !== newVersion.version) {
-    console.log('downloading:', newVersion.version);
-    const latestUrl = `http://storage.googleapis.com/fighter-html/${newVersion.version}.zip`;
-    const latestResp = await fetch(latestUrl);
-    const latestRaw = await latestResp.blob();
-    await fs.writeFile('tmp/game.zip', latestRaw);
+    await downloadDist(newVersion.version);
+    await unzipDist();
   } else {
     console.log('already up to date');
   }
 
-  await fs.writeFile('latest/version.json', newVersionRaw);
+  console.log('updating version.json');
+  await fsPromises.writeFile(path.latestVersion, newVersionRaw);
+  console.log('done!');
 };
 fetchVersion();
 
